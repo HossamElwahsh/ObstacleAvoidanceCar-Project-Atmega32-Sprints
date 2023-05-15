d5f07be970ea9fd388b919b448ade34a79deef85
Hossam elwahsh <hacker2894@gmail.com>
2023-05-15 16:05:24 +0300

    init commit

---
+++ Obstacle_Avoidance_Car_Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * ObstacleAvoidanceCar.c
+ *
+ * Created: 15/5/2023 3:47:05 PM
+ * Author : hossam
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
