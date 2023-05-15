e9331c9380f8719a5b82dfc58fdcf3dbe8838d2c
Hossam elwahsh <hacker2894@gmail.com>
2023-05-15 16:31:20 +0300

    add (icu): ICU interface header file

---
+++ Obstacle_Avoidance_Car_Project/ECUAL/icu/icu_interface.h
@@ -0,0 +1,15 @@
+/*
+ * icu_interface.h
+ *
+ * Created: 15/5/2023 4:24:07 PM
+ *  Author: hossam
+ */ 
+
+
+#ifndef ICU_INTERFACE_H_
+#define ICU_INTERFACE_H_
+
+void ICU_init(echo pin, callback);
+void ICU_getCaptureValue(void);
+
+#endif /* ICU_INTERFACE_H_ */
