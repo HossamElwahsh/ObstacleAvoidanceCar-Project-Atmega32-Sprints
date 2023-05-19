02217c1a55f633c97113ff16d1634a37e9040b3d
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-05-19 19:42:08 +0300

    update (DCM - Delay - PWM - Timer): Adding APIs descriptions

--- Obstacle_Avoidance_Car_Project/ECUAL/dcm/dcm_interface.h
+++ Obstacle_Avoidance_Car_Project/ECUAL/dcm/dcm_interface.h
@@ -69,14 +69,66 @@ typedef struct
 /**********************************************************************************************************************
  *  GLOBAL FUNCTION PROTOTYPES
  *********************************************************************************************************************/
+/******************************************************************************
+* @Syntax          : en_DCM_error_t DCM_init (void)
+* @Description     : Initializes DCM module
+* @Sync\Async      : Synchronous
+* @Reentrancy      : Reentrant
+* @Parameters (in) : None
+* @Parameters (out): None
+* @Return value:   : en_DCM_error_t			DCM_OK = 0
+*											DCM_NOK = 1
+*******************************************************************************/
 en_DCM_error_t DCM_init				(void);
 
+/******************************************************************************
+* @Syntax          : en_DCM_error_t DCM_setDirection	
+*					 (en_DCM_number_t en_a_dcmNumber, en_DCM_direction_t en_a_direction)
+* @Description     : Sets Directions for the a specific DCM
+* @Sync\Async      : Synchronous
+* @Reentrancy      : Reentrant
+* @Parameters (in) : en_DCM_number_t		en_a_dcmNumber
+*					 en_DCM_direction_t		en_a_direction
+* @Parameters (out): None
+* @Return value:   : en_DCM_error_t			DCM_OK = 0
+*											DCM_NOK = 1
+*******************************************************************************/
 en_DCM_error_t DCM_setDirection		(en_DCM_number_t en_a_dcmNumber, en_DCM_direction_t en_a_direction);
 
+/******************************************************************************
+* @Syntax          : en_DCM_error_t DCM_speed (u8 u8_a_speed)
+* @Description     : Sets speed for DCMs
+* @Sync\Async      : Synchronous
+* @Reentrancy      : Reentrant
+* @Parameters (in) : u8						u8_a_speed
+* @Parameters (out): None
+* @Return value:   : en_DCM_error_t			DCM_OK = 0
+*											DCM_NOK = 1
+*******************************************************************************/
 en_DCM_error_t DCM_speed			(u8 u8_a_speed);
 
+/******************************************************************************
+* @Syntax          : en_DCM_error_t DCM_start (void)
+* @Description     : Starts DCMs to rotate
+* @Sync\Async      : Synchronous
+* @Reentrancy      : Reentrant
+* @Parameters (in) : None
+* @Parameters (out): None
+* @Return value:   : en_DCM_error_t			DCM_OK = 0
+*											DCM_NOK = 1
+*******************************************************************************/
 en_DCM_error_t DCM_start			(void);
 
+/******************************************************************************
+* @Syntax          : en_DCM_error_t DCM_stop (void)
+* @Description     : Stops DCMs from rotating
+* @Sync\Async      : Synchronous
+* @Reentrancy      : Reentrant
+* @Parameters (in) : None
+* @Parameters (out): None
+* @Return value:   : en_DCM_error_t			DCM_OK = 0
+*											DCM_NOK = 1
+*******************************************************************************/
 en_DCM_error_t DCM_stop				(void);
 
 
